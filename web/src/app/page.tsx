75de2914e3675497a56efc44bf8b148ab50a6a47
cassight <cassightxl@protonmail.com>
2024-05-09 16:29:41 -0700

    add password for front page

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,12 +1,45 @@
 "use client";
+import React, { useState } from "react";
 import { Footer } from "@/app/components/footer";
 import { Logo } from "@/app/components/logo";
 import { PresetQuery } from "@/app/components/preset-query";
 import { Search } from "@/app/components/search";
-import React from "react";
 import { AllLangs, ALL_LANG_OPTIONS, changeLang, getLang } from "./locales";
 
 export default function Home() {
+  const [password, setPassword] = useState("");
+  const [isPasswordCorrect, setIsPasswordCorrect] = useState(false);
+
+  const handlePasswordSubmit = () => {
+    if (password === process.env.password) {
+      setIsPasswordCorrect(true);
+    } else {
+      alert("Incorrect password. Please try again.");
+    }
+  };
+
+  if (!isPasswordCorrect) {
+    return (
+      <div className="absolute inset-0 min-h-[500px] flex items-center justify-center">
+        <div className="relative flex flex-col gap-8 px-4">
+          <h2 className="text-2xl font-bold">Enter Password</h2>
+          <input
+            type="password"
+            className="rounded border border-gray-300 text-zinc-400 bg-white p-2 shadow-sm text-sm"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button
+            className="rounded bg-blue-500 text-white px-4 py-2"
+            onClick={handlePasswordSubmit}
+          >
+            Submit
+          </button>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="absolute inset-0 min-h-[500px] flex items-center justify-center">
       {/* 语言下拉菜单 */}
